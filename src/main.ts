9e3d57b84912ba804e537d41163de45d2e1215d6
UT01317 <UT01317@UNICOMTIC24>
2024-11-14 11:06:01 +0530

    Registration Form Image upload pending

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+
+bootstrapApplication(AppComponent, {
+  providers: [...appConfig.providers, provideHttpClient(),provideAnimationsAsync(), 
+  provideToastr(), provideAnimationsAsync(),]
+});
