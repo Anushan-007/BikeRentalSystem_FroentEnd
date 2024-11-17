5f3ee43f24be637150c488753100ad9158656145
UT01317 <UT01317@UNICOMTIC24>
2024-11-17 09:54:58 +0530

    login component begin stage

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,14 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, HttpClientModule],
+  imports: [RouterLink, HttpClientModule, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!:FormGroup;
+
+
+
+  constructor(private fb:FormBuilder, private router:Router){
+
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      userName: ['', [Validators.required]],
+      passwordHash: ['', [Validators.required]],
+    });
+  }
+
+
+
+  onSubmit(loginForm:any){
+
+  }
+
 
 }
