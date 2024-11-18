67134cf147f564f9c48a79731fb051d7c1bb4065
UT01317 <UT01317@UNICOMTIC24>
2024-11-18 10:54:09 +0530

    Admin Dashboard Enhance

--- src/app/AdminDashBoard/dash-board/dash-board.component.ts
+++ src/app/AdminDashBoard/dash-board/dash-board.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { BikeTableComponent } from '../bike-table/bike-table.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dash-board',
   standalone: true,
-  imports: [],
+  imports: [BikeTableComponent, RouterLink, RouterOutlet],
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.css'
 })
 export class DashBoardComponent {
+
+  toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar?.classList.toggle('active');
+  }
 
 }
