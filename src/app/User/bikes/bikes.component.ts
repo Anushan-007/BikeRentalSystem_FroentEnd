38276436ddcbe6f6e41f35c2741b324975038ef5
UT01317 <UT01317@UNICOMTIC24>
2024-11-21 19:01:00 +0530

    rental dropdown function

--- src/app/User/bikes/bikes.component.ts
+++ src/app/User/bikes/bikes.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Bike } from '../../Models/bike';
+import { BikeTableService } from '../../Services/bike-table.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bikes',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './bikes.component.html',
   styleUrl: './bikes.component.css'
 })
 export class BikesComponent {
+
+  bikesArray:Bike[] = [];
+
+  constructor(private biketableService:BikeTableService , private toastr :ToastrService){
+
+  }
+
+  ngOnInit(): void {
+    this.getBikes();
+  }
+
+
+  getBikes(){
+    this.biketableService.getAllBikes().subscribe({
+      next: (data) => {
+        console.log(data);
+        
+        this.bikesArray = data;
+      },
+      error: (err) => {
+        this.toastr.error("Response Error" + err)
+      }
+  })
+}
 
 }
