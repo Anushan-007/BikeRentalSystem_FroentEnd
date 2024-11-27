20ee10e3a3344376c0e9d907f57d66d1984c3efb
UT01317 <UT01317@UNICOMTIC24>
2024-11-27 11:22:39 +0530

    rental record

---
+++ src/app/Models/rentalRecord.ts
@@ -0,0 +1,24 @@
+import { bikeUnits } from "./bikeUnit";
+import { rentalRequest } from "./rentalRequest";
+
+export class rentalRecord{
+    id:string;
+    rentalOut?:string;
+    rentalReturn?:string;
+    payment?:string;
+    registrationNumber:string;
+    rentalRequestId :string;
+    unitId?:string;
+    bikeUnit?:bikeUnits[];
+    rentalRequest?:rentalRequest[];
+
+    constructor(obj:any){
+        this.id = obj.id != null ? obj.id :null;
+        this.rentalOut = obj.rentalOut != null ? obj.rentalOut :null;
+        this.payment = obj.payment != null ? obj.payment :null;
+        this.registrationNumber = obj.registrationNumber != null ? obj.registrationNumber :null;
+        this.rentalRequestId = obj.rentalRequestId != null ? obj.rentalRequestId :null;
+        this.unitId = obj.unitId != null ? obj.unitId :null;
+
+    }
+}
